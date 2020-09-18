39b1975fad1aaeb481228c38b54f0a0b5dd5089f
QuinnDur <71420962+QuinnDur@users.noreply.github.com>
2020-09-18 10:02:27 -0700

    Made my array of objects

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -1,15 +1,21 @@
 
 //create an empty array called balls
 
+let squares= [];
 let balls = [];
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1000, 1000);
 
 }
 
 function draw(){
-	background(220, 130, 90);
+	background(200, 140, 100);
+
+  for (let i = 0; i < squares.length; i++) {
+      squares[i].drawSquare();
+      squares[i].moveSquare();
+    }
 
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
@@ -18,30 +24,57 @@ function draw(){
 	  }
 }
 
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(random(0,800), random(0,400));
+function keyPressed(){
+  if (keyCode === UP_ARROW) {
+  let  b = new Ball(random(0,1000), random(0,1000));
   balls.push(b);
-  console.log(balls);
+  print(balls);
 }
+  if (keyCode === DOWN_ARROW) {
+  let  s = new Square(random(0,1000), random(0,1000));
+  squares.push(s);
+  print(squares);
+}
+}
+
 
 //ball class from which to create new balls with similar properties.
 class Ball {
-
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y){//every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
+
 	}
 
 	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(random(0,270), random(0,270), random(0,270));
-		    ellipse(this.x,this.y,10,10);
+    stroke(0);
+    fill(random(0,270), random(0,270), random(0,270));
+    ellipse(this.x,this.y,40,40);
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+random(-2,2);
-		this.y = this.y+random(-0.5,0.5);
+    this.x = this.x+random(-5,5);
+		this.y = this.y+random(-2,2);
 	}
 
 
+}
+
+class Square {
+	constructor(x,y){//every ball needs an x value and a y value
+		    this.x = x;
+    		this.y = y;
+
+	}
+
+	drawSquare(){  // draw a ball on the screen at x,y
+    stroke(0);
+    fill(random(0,270), random(0,270), random(0,270));
+    rect(this.x,this.y,50,50);
+	}
+
+	moveSquare(){ //update the location of the ball, so it moves across the screen
+    this.x = this.x+random(-4,4);
+		this.y = this.y+random(-3,3);
+	}
 }
